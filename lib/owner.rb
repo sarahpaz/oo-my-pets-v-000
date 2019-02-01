c8a6120a3eedc8ac04e93de18cb38214bc6b81f5
Sarah Paz <sarah.paz@gmail.com>
2019-02-01 15:04:29 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,6 +69,14 @@ class Owner
     @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
+  def sell_pets
+    @pets.each do |type, pets|
+      pets.clear
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+  end
 
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
